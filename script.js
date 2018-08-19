d3ae1bb1c68d779d73915ce071cc85c53214d70a
Sergey <jetFighterrr>
2018-08-18 22:21:57 -0700

    added form

--- script.js
+++ script.js
@@ -1,12 +1,57 @@
 // getHttpList();
 // render();
-getHttpList();
+// getHttpList();
 
-function render(){
-    let viewer = document.createElement('div');
-    viewer.setAttribute('class','viewer');
-    document.body.appendChild(viewer);
-    
+String.prototype.makeName = function(){
+  return (this[0].toUpperCase() + this.slice(1));
+};
+
+function work(){
+    // getHttpList()
+    //   .then(render(resolve))
+    //   .catch( console.log(reject) );
+  render(getHttpList());
+}
+
+function render(lst){
+  document.getElementsByTagName('div').innerHTML = '';
+
+  let viewer = document.createElement('div');
+
+  viewer.setAttribute('class','viewer');
+
+  let innerDiv = document.createElement('div');
+  innerDiv .setAttribute('class','img');
+
+
+  let image = document.createElement('img');
+  image.setAttribute('src',lst.picture.large);
+  image.setAttribute('class',100);
+  // image.setAttribute('height',100);
+  innerDiv.appendChild(image);
+
+  viewer.appendChild(innerDiv);
+
+  let infoWatch = document.createElement('div');
+  infoWatch.appendChild(createUserInfo('Name',  lst.name.title.makeName() + ' '
+                                              + lst.name.first.makeName() + ' '
+                                              + lst.name.last.makeName()));
+  infoWatch.appendChild(createUserInfo('Phone Number', lst.phone.toString()));
+  infoWatch.appendChild(createUserInfo('e-mail', lst.email));
+  infoWatch.appendChild(createUserInfo('Gender', lst.gender.toString().makeName()));
+  viewer.appendChild(infoWatch);
+
+
+
+
+  document.body.appendChild(viewer);
+}
+
+function createUserInfo(field, text){
+  let webInfo = document.createElement('p');
+  webInfo.setAttribute('class','info');
+  webInfo.innerHTML = field + ': ' + text;
+  return webInfo;
 }
 
 function getHttpList(){
@@ -20,9 +65,10 @@ function getHttpList(){
   console.log(response);
   // if result.
 
-  return new Promise((resolve,reject)=>{
-    if (response.results[0].gender === 'female') { resolve(response.results[0])}
-    else {reject('nothing found');}
-  })
+  return response.results[0];
+  // return new Promise((resolve,reject)=>{
+  //   if (response.results[0].gender === 'female') { resolve(response.results[0])}
+  //   reject('nothing found');
+  // })
 
 }
