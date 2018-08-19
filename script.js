ff34ce37f90f38f156f921f15c0d929d09c92fdf
Sergey <jetFighterrr>
2018-08-18 23:53:57 -0700

    added form

--- script.js
+++ script.js
@@ -1,34 +1,56 @@
-// getHttpList();
-// render();
-// getHttpList();
-
 String.prototype.makeName = function(){
-  return (this[0].toUpperCase() + this.slice(1));
+  return ( this[0].toUpperCase() + this.slice(1) );
 };
 
 function work(){
-    // getHttpList()
-    //   .then(render(resolve))
-    //   .catch( console.log(reject) );
-  render(getHttpList());
+  let viewer = document.getElementById('main');
+  viewer.innerHTML = '';
+  viewer.setAttribute('class', '');
+  // <div class="loader"></div>
+  let spinner = document.createElement('div');
+  spinner.setAttribute('class','loader');
+  viewer.appendChild(spinner);
+
+  setTimeout(call, 1000 + Math.ceil(Math.random()*1000) );
+  // render(getHttpList());
+}
+
+function call(){
+  getHttpList()
+    .then((resolve) => render(resolve))
+    .catch( (reject) => renderError(reject));
+}
+
+function renderError(message) {
+  let viewer = document.getElementById('main');
+  viewer.innerHTML = '';
+  viewer.setAttribute('class', 'viewer small');
+
+  let infoWatch = document.createElement('div');
+  infoWatch.setAttribute('class','plain');
+  infoWatch.appendChild(createUserInfo('Sorry,',message));
+  viewer.appendChild(infoWatch);
+
+  document.body.appendChild(viewer);
 }
 
 function render(lst){
   let viewer = document.getElementById('main');
   viewer.innerHTML = '';
-  viewer.setAttribute('class','viewer');
+  viewer.setAttribute('class','viewer big');
 
   viewer.appendChild(createUserPic(lst.picture.large));
 
   let infoWatch = document.createElement('div');
-  infoWatch.appendChild(createUserInfo('Name',  lst.name.title.makeName() + ' '
+  infoWatch.setAttribute('class','plain');
+  infoWatch.appendChild(createUserInfo('Name:',  lst.name.title.makeName() + ' '
                                               + lst.name.first.makeName() + ' '
                                               + lst.name.last.makeName()));
-  infoWatch.appendChild(createUserInfo('Phone Number', lst.phone.toString()));
-  infoWatch.appendChild(createUserInfo('E-mail', lst.email));
-  infoWatch.appendChild(createUserInfo('Gender', lst.gender.toString().makeName()));
-  infoWatch.appendChild(createUserInfo('City',lst.location.city.makeName()));
-  infoWatch.appendChild(createUserInfo('State',lst.location.state.makeName()));
+  infoWatch.appendChild(createUserInfo('Phone Number:', lst.phone.toString()));
+  infoWatch.appendChild(createUserInfo('E-mail:', lst.email));
+  infoWatch.appendChild(createUserInfo('Gender:', lst.gender.toString().makeName()));
+  infoWatch.appendChild(createUserInfo('City:',lst.location.city.makeName()));
+  infoWatch.appendChild(createUserInfo('State:',lst.location.state.makeName()));
   viewer.appendChild(infoWatch);
 
   document.body.appendChild(viewer);
@@ -37,7 +59,7 @@ function render(lst){
 function createUserInfo(field, text){
   let webInfo = document.createElement('p');
   webInfo.setAttribute('class','info');
-  webInfo.innerHTML = field + ': ' + text;
+  webInfo.innerHTML = field + ' ' + text;
   return webInfo;
 }
 
@@ -58,15 +80,13 @@ function getHttpList(){
   newQuery.send( null );
 
   let response = JSON.parse(newQuery.response);
-  console.log(response.results[0].gender);
 
-  console.log(response);
-  // if result.
-
-  return response.results[0];
-  // return new Promise((resolve,reject)=>{
-  //   if (response.results[0].gender === 'female') { resolve(response.results[0])}
-  //   reject('nothing found');
-  // })
+  // return response.results[0];
+  return new Promise((resolve,reject)=>{
+    if ( Math.random() > 0.15) {
+      return reject('something went wrong, try again later');
+    }
+    return resolve(response.results[0]);
+  })
 
 }
